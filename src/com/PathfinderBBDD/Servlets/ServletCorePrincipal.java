2a030f9d441960d6667a8b37c9c3e9b077fbbb14
Biblioteca 313 <Biblioteca 313@192.168.1.47>
2018-05-14 10:01:35 +0200

    ¡¡¡¡NO DESCARGAR!!!!: -Esta version solo esta para asegurar la importacion de metodos en 	ServletCorePrincipal important Calculadora y usando su metodo

--- src/com/PathfinderBBDD/Servlets/ServletCorePrincipal.java
+++ src/com/PathfinderBBDD/Servlets/ServletCorePrincipal.java
@@ -1,7 +1,11 @@
 package com.PathfinderBBDD.Servlets;
 
+import com.PathfinderBBDD.Funciones.Calculadora;
+
+
 
 import java.io.IOException;
+
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -15,11 +19,14 @@ import javax.servlet.http.HttpServletResponse;
 public class ServletCorePrincipal extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-	
+
 
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+		Calculadora MiCalculadora = new Calculadora();
+		
+		
 		String elComando = request.getParameter("instruccion");
 		
 		System.out.println("elComando: " + elComando);
@@ -29,7 +36,12 @@ public class ServletCorePrincipal extends HttpServlet {
 		
 		try {
 			switch(elComando) {
+			
+			
 			case "Login":
+				System.out.println("Vamos a sumar 3 y 4");
+				int Resultado = MiCalculadora.Suma(3,4);
+				System.out.println(Resultado);
 				HaciaPantallaLogin(request,response);
 			break;
 			case "VerificarUsuario":
